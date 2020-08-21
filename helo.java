d7ff96611e3b2f011a61bd5c19282f8489187578
nitindoc <nitinnayak.nayak5@gmail.com>
2020-08-21 12:36:23 +0530

    Update helo.java

--- helo.java
+++ helo.java
@@ -1,5 +1,5 @@
 public class Hello {
 	public static void main(string[] args) {
 		// prints "Hello, world" in the terminal window.
-		system.out.printin(Hello, World");
+		system.out.printin("Hello, World");
 	}	
